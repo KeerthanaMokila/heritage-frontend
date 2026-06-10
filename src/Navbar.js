0a0507ceafbb39d4da996e58e9efa1ec45db92b4
Keerthana <keerthanamokila3@gmail.com>
2026-06-10 13:26:04 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div
+      style={{
+        background: "#1e293b",
+        color: "white",
+        padding: "15px",
+      }}
+    >
+      <h2>🏛️ HeritageAR</h2>
+    </div>
+  );
+}
+
+export default Navbar;
